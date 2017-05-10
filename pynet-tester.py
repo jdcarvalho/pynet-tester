7cc1321261d27332150ab1ba79e0785cc68b1e3e
joao@bjsoftware.com.br <joao@bjsoftware.com.br>
2017-05-10 13:59:26 -0300

    First release

--- pynet-tester.py
+++ pynet-tester.py
@@ -10,10 +10,10 @@ HELP = """
     
     Usage:
     
-    Show this help message: "pynet-tester --help" o "pynet-tester -h"
-    To list all disponible servers: "pynet-tester --list" or "pynet-tester -l"
-    Check Internet Speed: "pynet-tester --check" or "pynet-tester --check SERVER_ID". (If you want a specific server to connect pass it here)
-    Export results: "pynet-tester --export YYYY-MM-DD" or "pynet-tester -e YYYY-MM-DD"
+    * Show this help message: "pynet-tester --help" o "pynet-tester -h"
+    * To list all disponible servers: "pynet-tester --list" or "pynet-tester -l"
+    * Check Internet Speed: "pynet-tester --check SERVER_ID" or "pynet-tester --c SERVER_ID".
+    * Export results: "pynet-tester --export YYYY-MM-DD" or "pynet-tester -e YYYY-MM-DD"
 
 """
 
